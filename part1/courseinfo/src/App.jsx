57a51069e70efafabca499d70e5543eaf3b8358b
antonym-92 <antonym.code92@gmail.com>
2023-08-28 10:32:09 +0300

    1.2 course information, step2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,18 +8,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   )
 }
@@ -52,6 +54,11 @@ const App = () => {
 
 Header.propTypes = {
   course: PropTypes.string.isRequired
+}
+
+Part.propTypes = {
+  part: PropTypes.string.isRequired,
+  exercises: PropTypes.number.isRequired
 }
 
 Content.propTypes = {
