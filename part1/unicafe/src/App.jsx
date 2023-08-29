047c6399a4b2a6d6a9901f984959ec2be0cfcdcd
antonym-92 <antonym.code92@gmail.com>
2023-08-29 15:47:37 +0300

    1.6: unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types'
 import { useState } from 'react'
 
-const Button = ({text, handleClick}) => (
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>
     {text}
   </button>
@@ -15,17 +17,28 @@ const App = () => {
   console.log(good, neutral, bad)
   return (
     <div>
-      <Button text={"good"} handleClick={() => setGood(good + 1)}/>
-      <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)}/>
-      <Button text={"bad"} handleClick={() => setBad(bad + 1)}/>
+      <Header text={"give feedback"} />
+      <Button text={"good"} handleClick={() => setGood(good + 1)} />
+      <Button text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
+      <Button text={"bad"} handleClick={() => setBad(bad + 1)} />
+      <Header text={"statistics"} />
+      <p>{`good ${good}`}</p>
+      <p>{`neutral ${neutral}`}</p>
+      <p>{`bad ${bad}`}</p>
     </div>
   )
+}
+
+Header.propTypes = {
+  text: PropTypes.string.isRequired
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   handleClick: PropTypes.func.isRequired
 }
+
+
 
 
 export default App
