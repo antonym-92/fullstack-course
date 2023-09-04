56439edda2fdbbb30121dc1db71fee53182ecb64
antonym-92 <antonym.code92@gmail.com>
2023-09-04 19:02:27 +0300

    2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { nanoid } from 'nanoid';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: nanoid(), name: 'Arto Hellas', }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const newPerson = {
+      id: nanoid(),
+      name: newName
+    }
+
+    setPersons([...persons, newPerson])
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
