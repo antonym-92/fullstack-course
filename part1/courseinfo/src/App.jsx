d5e2b05020a2d1fc36de308c77b31fe612c674cb
antonym-92 <antonym.code92@gmail.com>
2023-09-04 14:28:44 +0300

    2.5: separate module

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,60 +1,4 @@
-import PropTypes from 'prop-types';
-
-
-const Header = ({ header }) => {
-  return (
-    <h1>
-      {header}
-    </h1>
-  )
-}
-
-const Name = ({ name }) => {
-  return (
-    <h2>
-      {name}
-    </h2>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => <Part key={part.id} {...part} />)}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      <strong>
-        total of {
-          parts.map(part => part.exercises)
-            .reduce((prev, curr) => prev + curr, 0)} exercises
-      </strong>
-    </p>
-  )
-}
-
-const Course = ({ name, parts }) => {
-  return (
-    <>
-      <Name name={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </>
-  )
-}
-
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
@@ -104,42 +48,10 @@ const App = () => {
 
   return (
     <div>
-      <Header header='Web development curriculum' />
+      <h1>Web development curriculum</h1>
       {courses.map(course => <Course key={course.id} {...course} />)}
     </div>
   )
-}
-
-Header.propTypes = {
-  header: PropTypes.string.isRequired
-}
-
-Name.propTypes = {
-  name: PropTypes.string.isRequired
-}
-
-Part.propTypes = {
-  name: PropTypes.string.isRequired,
-  exercises: PropTypes.number.isRequired
-}
-
-const partsPropTypes = PropTypes.arrayOf(PropTypes.exact({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  exercises: PropTypes.number.isRequired
-})).isRequired
-
-Content.propTypes = {
-  parts: partsPropTypes
-}
-
-Total.propTypes = {
-  parts: partsPropTypes
-}
-
-Course.propTypes = {
-  name: PropTypes.string.isRequired,
-  parts: partsPropTypes
 }
 
 export default App
