8b15607a5bf7b6a26605897d87b91ae74d129bf8
antonym-92 <antonym.code92@gmail.com>
2023-09-04 19:26:47 +0300

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,20 +3,25 @@ import { nanoid } from 'nanoid';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: nanoid(), name: 'Arto Hellas', }
+    { id: nanoid(), name: 'Arto Hellas', number: '1234567' }
   ])
-  
+
   const [newName, setNewName] = useState('')
+
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    if(persons.find(x => x.name === newName))
+    if (persons.find(x => x.name === newName)){
       alert(`${newName} is already added to phonebook`)
-
+      return
+    }
+      
     const newPerson = {
       id: nanoid(),
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons([...persons, newPerson])
@@ -30,11 +35,14 @@ const App = () => {
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.id}>{person.name}</p>)}
+      {persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
